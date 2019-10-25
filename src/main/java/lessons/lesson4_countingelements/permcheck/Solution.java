1440f12caaceeab5a0c3dddd7e2ee7bc8f6aad79
Guilherme B. Pinheiro <guilhermepinheiro@gmail.com>
2019-10-24 21:32:01 -0300

    Lesson 4, Counting Elements, PermCheck.

---
+++ src/main/java/lessons/lesson4_countingelements/permcheck/Solution.java
@@ -0,0 +1,123 @@
+package lessons.lesson4_countingelements.permcheck;
+
+/**
+ * PermCheck
+ * <p>
+ * Check whether array A is a permutation.
+ * <p>
+ * A non-empty array A consisting of N integers is given.
+ * <p>
+ * A permutation is a sequence containing each element from 1 to N once, and only once.
+ * <p>
+ * For example, array A such that:
+ * A[0] = 4
+ * A[1] = 1
+ * A[2] = 3
+ * A[3] = 2
+ * <p>
+ * is a permutation, but array A such that:
+ * A[0] = 4
+ * A[1] = 1
+ * A[2] = 3
+ * <p>
+ * is not a permutation, because value 2 is missing.
+ * <p>
+ * The goal is to check whether array A is a permutation.
+ * <p>
+ * Write a function:
+ * <p>
+ * class Solution { public int solution(int[] A); }
+ * <p>
+ * that, given an array A, returns 1 if array A is a permutation and 0 if it is not.
+ * <p>
+ * For example, given array A such that:
+ * A[0] = 4
+ * A[1] = 1
+ * A[2] = 3
+ * A[3] = 2
+ * <p>
+ * the function should return 1.
+ * <p>
+ * Given array A such that:
+ * A[0] = 4
+ * A[1] = 1
+ * A[2] = 3
+ * <p>
+ * the function should return 0.
+ * <p>
+ * Write an efficient algorithm for the following assumptions:
+ * <p>
+ * N is an integer within the range [1..100,000];
+ * each element of array A is an integer within the range [1..1,000,000,000].
+ * <p>
+ * Copyright 2009–2019 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+ */
+
+public class Solution {
+
+    public static final int MAX_LENGTH = 100000;
+    public static final int MIN_LENGTH = 1;
+
+    public int solution(int[] A) {
+        if (hasArrayCorrectLengthAndIsEveryElementWithinRange(A)
+                && isEachElementPresentOnceAndOnlyOnce(A)
+        ) {
+            return 1;
+        }
+        return 0;
+    }
+
+    private boolean hasArrayCorrectLengthAndIsEveryElementWithinRange(int[] A) {
+
+        if (A.length < MIN_LENGTH
+                || A.length > MAX_LENGTH
+        ) {
+            return false;
+        }
+
+        int maxElement = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            if (!isElementWithinRange(A[i]))
+                return false;
+            if (A[i] > maxElement)
+                maxElement = A[i];
+        }
+        if (maxElement != A.length) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isElementWithinRange(int element) {
+        if (element >= 1
+                && element <= 1000000000) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isEachElementPresentOnceAndOnlyOnce(int[] A) {
+        int[] countArray = new int[A.length];
+        countElements(A, countArray);
+        for (int i = 0; i < countArray.length; i++) {
+            if (countArray[i] != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void countElements(int[] A, int[] countArray) {
+        initializeArrayWithZeroes(countArray);
+        for (int i = 0; i < A.length; i++) {
+            countArray[A[i] - 1] += 1;
+        }
+    }
+
+    private void initializeArrayWithZeroes(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 0;
+        }
+    }
+}
---
+++ src/test/java/lessons/lesson4_countingelements/permcheck/Tests.java
@@ -0,0 +1,94 @@
+package lessons.lesson4_countingelements.permcheck;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.*;
+
+public class Tests {
+
+    private static final long TIMEOUT_LIMIT = 6000;
+
+    Solution testSolution = new Solution();
+
+    @Test
+    public void example1Test() {
+        int[] input = {4, 1, 3, 2};
+        int expected = 1;
+        resultAndPerformanceTest(input,expected);
+    }
+
+    @Test
+    public void example2Test() {
+        int[] input = {4,1,3};
+        int expected = 0;
+        resultAndPerformanceTest(input,expected);
+    }
+
+    @Test
+    public void shouldReturnZeroIfArrayIsEmptyTest() {
+        int[] input = {};
+        int expected = 0;
+        resultAndPerformanceTest(input,expected);
+    }
+
+    @Test
+    public void shouldReturnZeroIfMinElementDifferentThanOneTest() {
+        int[] input = {2,4,3,5};
+        int expected = 0;
+        resultAndPerformanceTest(input,expected);
+    }
+
+    @Test
+    public void shouldReturnZeroIfArrayLengthGreaterThan100000Test() {
+        int[] input = new int[100001];
+        for (int i = 0; i < input.length; i++) {
+            input[i] = i+1;
+        }
+        int expected = 0;
+        resultAndPerformanceTest(input,expected);
+    }
+
+    @Test
+    public void shouldReturnZeroIfMaxElementDifferentThanArrayLengthTest() {
+        int[] input = {4,3,6,2,7,1};
+        int expected = 0;
+        resultAndPerformanceTest(input,expected);
+    }
+
+    @Test
+    public void shouldReturnZeroIfArrayDoesNotHaveEachElementOnceAndOnlyOnceTest() {
+        int[] input = {1,2,2,4,5};
+        int expected = 0;
+        resultAndPerformanceTest(input,expected);
+    }
+
+    @Test
+    public void shouldReturnOneIfArrayIsAPermutationTest() {
+        int[] input = {2,3,4,1,5};
+        int expected = 1;
+        resultAndPerformanceTest(input,expected);
+    }
+
+    @Test
+    public void shouldReturnOneIfArrayLengthEqualTo100000Test() {
+        int[] input = new int[100000];
+        for (int i = 0; i < input.length; i++) {
+            input[i] = i+1;
+        }
+        int expected = 1;
+        resultAndPerformanceTest(input,expected);
+    }
+
+    private void resultAndPerformanceTest(int[] input, int expected) {
+        long startTime = System.currentTimeMillis();
+        int actual = testSolution.solution(input);
+        long stopTime = System.currentTimeMillis();
+
+        long elapsedTime = stopTime - startTime;
+
+        Assert.assertThat(actual, is(equalTo(expected)));
+        Assert.assertThat("elapsed time", elapsedTime, is(lessThan(TIMEOUT_LIMIT)));
+    }
+}
